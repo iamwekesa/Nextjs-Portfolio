10af8e88dcb541de40f9370fef5d40174103d070
Dennis E. Wekesa <dennis.e.wekesa@gmail.com>
2023-03-22 09:35:23 +0300

    Page is under construction message

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,14 @@ import Navbar from './Navbar'
 const Header = () => {
   return (
     <>
+      <div className="mt-12 p-2 md:mt-0 text-center border-b bg-white dark:bg-black text-sm">
+        This page is under construction.
+      </div>
       <header className="md:pt-4 container max-w-screen-lg m-auto md:px-12 lg:px-20">
         <nav
           className="hidden md:flex justify-start items-center h-full mt-auto space-x-6 text-sm lg:justify-start backdrop-filter backdrop-blur-sm  bg-opacity-30"
           aria-label="Main Navigation"
         >
-          <Logo />
           <MenuItems />
           <ThemeChanger
             styles={
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,17 +2,16 @@ import Link from 'next/link'
 
 const Logo = () => {
   return (
-    (<Link
+    <Link
       href="/"
       aria-label="Website logo, go back to homepage."
-      className="flex items-center border-white group focus-visible:outline-accent">
-
+      className="flex items-center border-white group focus-visible:outline-accent"
+    >
       <div className="overflow-hidden transition ease-in-out rounded-full  hover:opacity-60">
-        <span className="text-sm">cosmicjs</span>
-        <span className="text-sm text-accent">.com</span>
+        <span className="text-sm">Wekesa</span>
+        <span className="text-sm text-accent">.dev</span>
       </div>
-
-    </Link>)
-  );
+    </Link>
+  )
 }
 export default Logo
